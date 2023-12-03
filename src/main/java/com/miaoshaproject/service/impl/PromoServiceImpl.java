9718614693972f4e74bf6689dd82c6f06af4ae9d
An <2582215315@qq.com>
2023-12-03 23:06:35 +0800

    分布式会话

--- src/main/java/com/miaoshaproject/service/impl/PromoServiceImpl.java
+++ src/main/java/com/miaoshaproject/service/impl/PromoServiceImpl.java
@@ -4,15 +4,22 @@ import com.miaoshaproject.dao.PromoDOMapper;
 import com.miaoshaproject.dataobject.PromoDO;
 import com.miaoshaproject.service.ItemService;
 import com.miaoshaproject.service.PromoService;
+import com.miaoshaproject.service.UserService;
 import com.miaoshaproject.service.model.ItemModel;
 import com.miaoshaproject.service.model.PromoModel;
+import com.miaoshaproject.service.model.UserModel;
 import org.joda.time.DateTime;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.math.BigDecimal;
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 
 @Service
@@ -25,7 +32,12 @@ public class PromoServiceImpl implements PromoService {
     private ItemService itemService;
 
     @Autowired
+    private UserService userService;
+
+    @Autowired
     private RedisTemplate redisTemplate;
+
+
 
     @Override
     public PromoModel getPromoByItemId(Integer itemId) {
@@ -61,7 +73,64 @@ public class PromoServiceImpl implements PromoService {
         //将库存同步到redis内
         redisTemplate.opsForValue().set("promo_item_stock_"+itemModel.getId(), itemModel.getStock());
 
+        //将大闸的限制数字设到redis内
+        redisTemplate.opsForValue().set("promo_door_count_"+promoId,itemModel.getStock().intValue() * 5);
+
     }
+
+
+    @Override
+    public String generateSecondKillToken(Integer promoId,Integer itemId,Integer userId) {
+
+        //判断是否库存已售罄，若对应的售罄key存在，则直接返回下单失败
+        if(redisTemplate.hasKey("promo_item_stock_invalid_"+itemId)){
+            return null;
+        }
+        PromoDO promoDO = promoDOMapper.selectByPrimaryKey(promoId);
+
+        //dataobject->model
+        PromoModel promoModel = convertFromDataObject(promoDO);
+        if(promoModel == null){
+            return null;
+        }
+
+        //判断当前时间是否秒杀活动即将开始或正在进行
+        if(promoModel.getStartDate().isAfterNow()){
+            promoModel.setStatus(1);
+        }else if(promoModel.getEndDate().isBeforeNow()){
+            promoModel.setStatus(3);
+        }else{
+            promoModel.setStatus(2);
+        }
+        //判断活动是否正在进行
+        if(promoModel.getStatus().intValue() != 2){
+            return null;
+        }
+        //判断item信息是否存在
+        ItemModel itemModel = itemService.getItemByIdInCache(itemId);
+        if(itemModel == null){
+            return null;
+        }
+        //判断用户信息是否存在
+        UserModel userModel = userService.getUserByIdInCache(userId);
+        if(userModel == null){
+            return null;
+        }
+
+        //获取秒杀大闸的count数量
+        long result = redisTemplate.opsForValue().increment("promo_door_count_"+promoId,-1);
+        if(result < 0){
+            return null;
+        }
+        //生成token并且存入redis内并给一个5分钟的有效期
+        String token = UUID.randomUUID().toString().replace("-","");
+
+        redisTemplate.opsForValue().set("promo_token_"+promoId+"_userid_"+userId+"_itemid_"+itemId,token);
+        redisTemplate.expire("promo_token_"+promoId+"_userid_"+userId+"_itemid_"+itemId,5, TimeUnit.MINUTES);
+
+        return token;
+    }
+
 
 
     private PromoModel convertFromDataObject(PromoDO promoDO){
